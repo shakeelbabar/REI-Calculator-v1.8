287c8f7307522e33ace6cc4625120c436b26be5e
myname <my@email.com>
2020-04-22 10:14:50 +0200

    Defined variables used inside run_calculations function

--- main.py
+++ main.py
@@ -72,12 +72,25 @@ class SettingsWindow(QDialog):
 
 
 class CalculatorWindow(QtWidgets.QMainWindow):  
+    
+    selling_costs = 0
+    int_rate = 0
+    loan_amount_auto = 0
+    prop_appreciation = 0
+    arv = 0
+    repair_and_maintenance = 0
+    rent_appreciation = 0
+    exp_appreciation = 0
+    tot_monthly_income = 0
+    
     # purchase information
     purchase_price = 0
     rehab_budget = 0
     closing_costs = 0
     finance_rehab_logical = 0
     emergency_fund = 0
+    downpayment = 0
+    term_years = 0
     
     # fixed expenses
     electric = 0
@@ -102,8 +115,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
     rental_income_monthly = 0
     
     # assumptions 
-    downpayment = 0
-    term_years = 0
+    
     
     
     def __init__(self):
@@ -196,7 +208,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
         ## Will then build a loop for the Pro forma statement.
 
         # Begin by calculating the monthly payment of the loan
-        payment = REI_Calculations.loan_payment(rate, term, present_value)
+        payment = REI_Calculations.loan_payment(self.rate, self.term, self.present_value)
 
         # Each item that is to be showcased will have a number before it.
         # only 8 are being calculated here.  There are an additional 4
@@ -212,50 +224,53 @@ class CalculatorWindow(QtWidgets.QMainWindow):
 
         # Calculate the fixed and variable expenses
         fixed_monthly, fixed_yearly = REI_Calculations.fixed_expenses_monthly(
-            electric, WandS, PMI, garbage, HOA, Insurance, taxes, other
+            self.electric, self.WandS, self.PMI, 
+            self.garbage, self.HOA, self.insurance, 
+            self.taxes, self.other
         )
 
         var_monthly, var_yearly = REI_Calculations.variable_expenses_monthly(
-            total_income_monthly, vacancy, rep_and_main, cap_ex, management
+            self.total_income_monthly, self.vacancy, self.rep_and_main, 
+            self.cap_ex, self.management
         )
 
         # 1 Calculate NOI
         NOI = REI_Calculations.NOI_yearly(
-            total_income_monthly * 12, fixed_yearly, var_yearly
+            self.total_income_monthly * 12, fixed_yearly, var_yearly
         )
 
         # 2,3 Calculate Monthly and yearly NIAF or monthly cash flows
         cf_monthly = REI_Calculations.cash_flow_monthly(
-            total_income_monthly, payment, fixed_monthly, var_monthly
+            self.total_income_monthly, payment, fixed_monthly, var_monthly
         )
         NIAF = REI_Calculations.NIAF_yearly(NOI, payment)
 
         # 4 Calculate the cash to close
         cash_2_close = REI_Calculations.cash_to_close(
-            downpayment,
-            closing_costs,
-            emergency_fund,
-            rehab_budget,
-            finance_rehab_logical,
+            self.downpayment,
+            self.closing_costs,
+            self.emergency_fund,
+            self.rehab_budget,
+            self.finance_rehab_logical,
         )
 
         # 5 Calculate Cash on Cash Return (CoCR)
         CoCR = REI_Calculations.cash_on_cash_return(NIAF, cash_2_close)
 
         # 6 Calculate the cap rate
-        capRate = REI_Calculations.cap_rate(NOI, purchase_price, closing_costs)
+        capRate = REI_Calculations.cap_rate(NOI, self.purchase_price, self.closing_costs)
 
         # 7 Calculate Gross Rent Multiplier
         GRM = REI_Calculations.gross_rent_mult(
-            purchase_price, rehab_budget, total_income_monthly
+            self.purchase_price, self.rehab_budget, self.total_income_monthly
         )
 
         # 8,9 Calculate the 1 and 50 percent rules
         one_perc = REI_Calculations.one_perc_rule(
-            purchase_price, rehab_budget, total_income_monthly
+            self.purchase_price, self.rehab_budget, self.total_income_monthly
         )
         fifty_perc = REI_Calculations.fifty_perc_rule(
-            fixed_monthly, var_monthly, total_income_monthly
+            fixed_monthly, var_monthly, self.total_income_monthly
         )
 
         ## Will need to store this data now as some of it may be overwritten
@@ -277,17 +292,17 @@ class CalculatorWindow(QtWidgets.QMainWindow):
 
             # Calculate total annual income for each year
             tot_income_annual = REI_Calculations.future_value(
-                rent_appreciation_input, year_index - 1, 0, tot_monthly_income
+                self.rent_appreciation, year_index - 1, 0, self.tot_monthly_income
             )
 
             # Calculate total fixed expenses for each year
             fixed_yearly = REI_Calculations.future_value(
-                exp_appreciation_input, year_index - 1, 0, fixed_monthly * 12
+                self.exp_appreciation, year_index - 1, 0, fixed_monthly * 12
             )
 
             # Calculate total variable expenses for each year
             var_yearly = tot_income_annual * sum(
-                r_and_m_input, cap_ex_input, vacancy_input, manag_input
+                self.repair_and_maintenance, self.cap_ex, self.vacancy, self.management
             )
 
             # Calculate Total Expenses
@@ -299,7 +314,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
             )
 
             # Calculate Debt service
-            if term_input < year_index:
+            if self.term < year_index:
                 payment = 0
             payment_annual = payment * 12
 
@@ -308,7 +323,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
 
             # Calculate Property Value
             prop_value = REI_Calculations.future_value(
-                prop_appreciation_input, year_index - 1, 0, arv_input
+                self.prop_appreciation, year_index - 1, 0, self.arv
             )
 
             # Calculate Cash on Cash Return
@@ -322,7 +337,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
 
             # Calculate future loan balance
             loan_balance = REI_Calculations.future_value(
-                int_rate_input / 12, year_index * 12, payment, loan_amount_auto
+                self.int_rate / 12, year_index * 12, payment, self.loan_amount_auto
             )
             # Calculate total equity owned by investor
             tot_equity = REI_Calculations.future_equity(prop_value, loan_balance)
@@ -337,7 +352,7 @@ class CalculatorWindow(QtWidgets.QMainWindow):
 
             # Calculate profit if sold
             total_profit_sold = REI_Calculations.tot_profit_sold(
-                prop_value, selling_costs_input, loan_balance, cash_2_close, cum_NIAF
+                prop_value, self.selling_costs, self.loan_balance, cash_2_close, cum_NIAF
             )
 
             # Calculate the compounded annual growth rate (CAGR) if sold
